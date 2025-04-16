0b8ecdcdaf2e0cc88a814b2d1e38dfa6090e96d4
Repal <96978162+Revalino-f@users.noreply.github.com>
2025-04-16 08:58:20 +0700

    Delete resources directory

--- resources/views/livewire/list-wish.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    @foreach ($wish as $item)
-    <div class="alert-box alert-box--notice">
-        <span><b>{{ $item->name }}</span></b>
-        <br>
-        <span>{{ $item->comment }}</span>
-    </div>
-    @endforeach
-</div>
